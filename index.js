3633ce4e1b1bc2482634fb8355de78c18478ab1d
Ethan <ethanadiem@gmail.com>
2025-02-04 12:13:43 -0700

    initial styling added to page, margins and sizes adjusted

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ document.getElementById("submitBtn").addEventListener("click", function() {
   const textValue = document.getElementById("textInput").value;
     if (textValue.trim() === "") return;
 
+  const wrapper = document.createElement("div");
+    wrapper.classList.add("tasktexttotheside");
+
   const NewButton = document.createElement("button");
     NewButton.textContent = " ";
+    NewButton.classList.add("tasks");
 
-  const textNode = document.createTextNode(" " + textValue);
+    const textNode = document.createElement("span");
+    textNode.textContent = textValue;
 
-  const container = document.getElementById("buttonContainer");
+  // const textNode = document.createTextNode(" " + textValue);
+
+  const container = document.getElementById("buttonContainer").appendChild(wrapper);
     container.appendChild(NewButton);
     container.appendChild(textNode);
+
+document.getElementById("buttonContainer").appendChild(wrapper);
 
 document.getElementById("textInput").value = "";
 
